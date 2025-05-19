6bff851da33ea1dec13ffde8a902e3a34c6ee868
LeoG <123471322+LeoGINERG22006196@users.noreply.github.com>
2025-05-19 16:34:07 +0200

    [AJOUT]ajout des pages

--- src/Controller/RedirectionController.php
+++ src/Controller/RedirectionController.php
@@ -8,11 +8,60 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class RedirectionController extends AbstractController
 {
-    #[Route('/redirection', name: 'app_redirection')]
-    public function index(): Response
+	#[Route('/ONG/Accueil', name: 'ONG_Accueil')]
+	public function ONGAccueil(): Response
+	{
+		return $this->render('ONGAccueil.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+	#[Route('/ONG/Carte', name: 'ONG_Carte')]
+	public function ONGCarte(): Response
+	{
+		return $this->render('ONGCarte.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+    #[Route('/ONG/Classement', name: 'ONG_Classement')]
+    public function ONGClassement(): Response
     {
-        return $this->render('redirection/index.html.twig', [
+        return $this->render('ONGClassement.html.twig', [
             'controller_name' => 'RedirectionController',
         ]);
     }
+	#[Route('/ONG/Compte', name: 'ONG_Compte')]
+	public function ONGCompte(): Response
+	{
+		return $this->render('ONGCompte.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+	#[Route('/ONG/Enregistrement', name: 'ONG_Enregistrement')]
+	public function ONGEnregistrement(): Response
+	{
+		return $this->render('ONGEnregistrement.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+	#[Route('/ONG/Forfait', name: 'ONG_Forfait')]
+	public function ONGForfait(): Response
+	{
+		return $this->render('ONGForfait.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+	#[Route('/ONG/ReseauxSocial', name: 'ONG_ReseauxSocial')]
+	public function ONGReseauxSocial(): Response
+	{
+		return $this->render('ONGReseauxSocial.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
+	#[Route('/ONG/TableauDeBord', name: 'ONG_TableauDeBord')]
+	public function ONGTableauDeBord(): Response
+	{
+		return $this->render('ONGTableauDeBord.html.twig', [
+			'controller_name' => 'RedirectionController',
+		]);
+	}
 }
