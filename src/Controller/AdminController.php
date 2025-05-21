211461405d270777c647db4a29752b4b814f0c9a
suricate <nathan.gouache@etu.univ-amu.fr>
2025-05-21 15:32:25 +0200

    Fix du base.html.twig et aussi j'ai rajouté la liste des especes dans un /admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\EspecePoisson;
+use App\Form\EspecePoissonTypeForm;
+use App\Repository\EspecePoissonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin/liste-especes', name: 'admin_liste_especes')]
+    public function ListeEspeces(EspecePoissonRepository $repo): Response
+    {
+
+        $especes = $repo->findAll();
+
+        return $this->render('admin/liste_especes.html.twig', [
+            'especes' => $especes
+        ]);
+    }
+
+    #[Route('/admin/creer-especes', name: 'admin_creer_espece')]
+    public function creerEspece(Request $request, EntityManagerInterface $em): Response
+    {
+        $espece = new EspecePoisson();
+        $form = $this->createForm(EspecePoissonTypeForm::class, $espece);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($espece);
+            $em->flush();
+
+            $this->addFlash('success', 'Nouvelle espèce ajoutée à la criée moussaillon !');
+            return $this->redirectToRoute('admin_liste_especes');
+        }
+
+        return $this->render('admin/creer_espece.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/supprimer-espece/{id}', name: 'admin_supprimer_espece', methods: ['POST'])]
+    public function supprimerEspece(EspecePoisson $poisson, EntityManagerInterface $em, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$poisson->getId(), $request->request->get('_token'))) {
+            $em->remove($poisson);
+            $em->flush();
+
+            $this->addFlash('success', 'Espèce supprimée avec succès capitaine !');
+        }
+
+        return $this->redirectToRoute('admin_liste_especes');
+    }
+
+}
