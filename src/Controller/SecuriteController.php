5b2159b88d0244de9968c60af151f333cb2e882a
Maxime TAMARIN <maxtam83390@gmail.com>
2025-05-21 15:13:21 +0200

    [ADD] formulaire de connexion

--- src/Controller/SecuriteController.php
+++ src/Controller/SecuriteController.php
@@ -8,11 +8,13 @@ use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
@@ -23,7 +25,7 @@ class SecuriteController extends AbstractController
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Security $security): Response
     {
         $user = new ONG();
         $form = $this->createForm(RegistrationForm::class, $user);
@@ -38,30 +40,32 @@ class SecuriteController extends AbstractController
 
             // on set les attributs manquant
             $user->setPoints(0);
-//            $user->setNomOng()
+            $user->setRoles(['ROLE_ONG']);
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('maxime.tamarin@etu.univ-amu.fr', 'Dive And Collect'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            // genere un mail avec un lien de verification (il faut configurer le DNS dans le .env)
+//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+//                (new TemplatedEmail())
+//                    ->from(new Address('maxime.tamarin@etu.univ-amu.fr', 'Dive And Collect'))
+//                    ->to((string) $user->getEmail())
+//                    ->subject('Please Confirm your Email')
+//                    ->htmlTemplate('registration/confirmation_email.html.twig')
+//            );
 
-            // do anything else you need here, like send an email
+            $this->addFlash('success', 'Inscription réussi');
+            $security->login($user);
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('ONG_Forfait');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('securite/register.html.twig', [
             'registrationForm' => $form,
         ]);
     }
 
+    // A UTILISER SI VERIFICATION DE MAIL MIS EN PLACE
     #[Route('/verify/email', name: 'app_verify_email')]
     public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
     {
@@ -78,9 +82,32 @@ class SecuriteController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse email est maintenant verifié.');
 
         return $this->redirectToRoute('ONG_Accueil');
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
+    {
+        if ($security->isGranted('ROLE_ONG'))
+            $this->redirectToRoute('ONG_Accueil');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('securite/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
